d1ab87968fff676a163e40edbef8408dd824af4a
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-06-28 08:50:55 +0800

    last

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+    outDir: 'build',
+  },
   server: {
     port:3004,
     strictPort: true,
   },
-  server: {
-    port: 3004,
-    strictPort: true,
-    host: true,
-    origin: 'http://0.0.0.0:3004'
   
-  },
+  // server: {
+  //   port: 3004,
+  //   strictPort: true,
+  //   host: true,
+  //   origin: 'http://0.0.0.0:3004'
+  
+  // },
 })
